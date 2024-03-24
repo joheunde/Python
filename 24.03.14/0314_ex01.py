d74b3a5508ffecec115a7dc423dcca1005d0d5f9
joheunde <164715808+joheunde@users.noreply.github.com>
2024-03-24 15:29:51 +0900

    Delete 24.03.14 directory

--- 24.03.14/0314_ex01.py
+++
@@ -1,7 +0,0 @@
-print("## 택배를 보내기 위한 정보를 입력하세요. ##")
-name = input("받는 사람 : ")
-address = input("주소 : ")
-weight = int(input("무게(g) : "))
-print(f"** 받는 사람 ==> {name}")
-print(f"** 주소 ==> {address}")
-print(f"** 배송비 ==> {weight * 5}원")
--- 24.03.14/0314_ex04.py
+++
@@ -1,4 +0,0 @@
-print("True and True :", True and True)
-print("True and False :", True and False)
-print("False and True :", False and True)
-print("False and False :", False and False)
