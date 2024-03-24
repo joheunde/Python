e63982faa3d2ef0eb12908b87fda1c9446e5a9e2
joheunde <164715808+joheunde@users.noreply.github.com>
2024-03-24 16:31:34 +0900

    Add files via upload

---
+++ GUI_project/GUI_basic/15_메모장.py
@@ -0,0 +1,47 @@
+import os
+from tkinter import *
+
+root = Tk()
+root.title("제목 없음 - Windows 메모장")
+root.geometry("640x480") # 가로 * 세로
+
+# 열기, 저장 파일 이름
+filename = "메모장.txt"
+
+def open_file():
+    if os.path.isfile(filename): # 파일 있으면 True, 없으면 False
+        with open(filename, "r", encoding="utf8") as file:
+            txt.delete("1.0", END) # 텍스트 위젯 본문 삭제
+            txt.insert(END, file.read()) # 파일 내용을 본문에 입력
+
+def save_file():
+    with open(filename, "w", encoding="utf8") as file:
+        file.write(txt.get("1.0", END)) # 모든 내용을 가져와서 저장
+
+menu = Menu(root)
+
+# 파일 메뉴
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="열기", command=open_file)
+menu_file.add_command(label="저장", command=save_file)
+menu_file.add_separator()
+menu_file.add_command(label="끝내기", command=root.quit)
+menu.add_cascade(label="파일", menu=menu_file)
+
+# 편집, 서식, 보기, 도움말
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
+
+# 스크롤 바
+scrollbar = Scrollbar(root)
+scrollbar.pack(side="right", fill="y")
+
+# 본문 영역
+txt = Text(root, yscrollcommand=scrollbar.set)
+txt.pack(side="left", fill="both", expand=True)
+scrollbar.config(command=txt.yview)
+
+root.config(menu=menu)
+root.mainloop()
---
+++ GUI_project/GUI_basic/9_porgressbar.py
@@ -0,0 +1,35 @@
+import time
+import tkinter.ttk as ttk
+from tkinter import *
+
+root = Tk()
+root.title("Nado GUI")
+root.geometry("640x480") # 가로 * 세로
+
+# progressbar = ttk.Progressbar(root, maximum=100, mode="indeterminate")
+# progressbar = ttk.Progressbar(root, maximum=100, mode="determinate")
+# progressbar.start(10) # 10ms 마다 움직임
+# progressbar.pack()
+
+# def btncmd():
+#     progressbar.stop() # 작동 중지
+
+# btn = Button(root, text="중지", command=btncmd)
+# btn.pack()
+
+p_var2 = DoubleVar()
+progressbar2 = ttk.Progressbar(root, maximum=100, length=150, variable=p_var2)
+progressbar2.pack()
+
+def btncmd2():
+    for i in range(1, 101): # 1 ~ 100
+        time.sleep(0.01) # 0.01초 대기
+        
+        p_var2.set(i) # progress bar의 값 설정
+        progressbar2.update() # ui 업데이트
+        print(p_var2.get())
+
+btn = Button(root, text="시작", command=btncmd2)
+btn.pack()
+
+root.mainloop()
