203fed0260d6fc5b8a91f7fd7c4bc5c4b866f00b
joheunde <164715808+joheunde@users.noreply.github.com>
2024-04-08 13:00:34 +0900

    Add files via upload

---
+++ Python_Class/24.04.08/240408_ex03.py
@@ -0,0 +1,8 @@
+my_list = []
+print(f"초기 리스트 : {my_list}")
+
+# (1) append(값) : 값을 리스트의 마지막에 추가한다.
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+print(f"append 이후 : {my_list}")
---
+++ Python_Class/24.04.08/240408_ex04.py
@@ -0,0 +1,17 @@
+# 음수 인덱스에 대한 내용
+
+my_list = [24, 54, 32, 16]
+
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+print(my_list[3])
+
+print("-----------------------")
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+print(my_list[-4])
+
+msg = "hello"
+print(msg[-1])
